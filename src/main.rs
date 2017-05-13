2b3bff3d8417429f522a8769fd0c060b5a8cb127
Haruhiko Uchida <harre.orz@gmail.com>
2017-05-13 18:26:18 +0900

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,80 @@
+extern crate base64;
+
+use std::io;
+use std::env;
+
+const FST: &'static str = "[{";
+const SEP: &'static str = ",";
+const LST: &'static str = ",{";
+
+fn base64_decode(value: &mut String) {
+    use base64::decode;
+
+    *value = if let Ok(value) = decode(value) {
+        if let Ok(value) = String::from_utf8(value) {
+            value
+        } else {
+            return
+        }
+    } else {
+        return
+    }
+}
+
+fn main() {
+    let collect_params: Vec<String> = env::args().skip(1).collect();
+
+    let mut line = String::new();
+    let mut value = String::new();
+    let mut state = FST;
+    let mut encoding = false;
+    let mut show = false;
+
+    loop {
+        line.clear();
+        match io::stdin().read_line(&mut line) {
+            Ok(0) | Err(_) => {
+                if state == LST {
+                    println!("]");
+                }
+                return;
+            },
+            Ok(1) => {
+                if state == SEP {
+                    if show {
+                        if encoding {
+                            base64_decode(&mut value);
+                        }
+                        print!("\"{}\"", value);
+                        value.clear();
+                    }
+                    print!("{}", '}');
+                    state = LST;
+                }
+            },
+            Ok(n) => {
+                let line = &line[..n];
+                if line.starts_with('#') {
+                    // do nothing
+                } else if !line.starts_with(' ') {
+                    if state == SEP && show {
+                        if encoding {
+                            base64_decode(&mut value);
+                        }
+                        print!("\"{}\"", value);
+                    }
+                    let (key, val) = line.split_at(line.find(':').unwrap());
+                    show = collect_params.is_empty() || collect_params.iter().any(|x| x == key);
+                    if show {
+                        print!("{}\"{}\":", state, key);
+                        encoding = val.starts_with("::");
+                        value = val[if encoding { 2 } else { 1 }..].trim().to_string();
+                        state = SEP;
+                    }
+                } else {
+                    value += line.trim();
+                }
+            },
+        }
+    }
+}
