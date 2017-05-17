12aeeb7f83577ef2a591c721b39678801409e021
Haruhiko Uchida <harre.orz@gmail.com>
2017-05-17 20:10:02 +0900

    supports continuous name

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use std::env;
 const FST: &'static str = "[{";
 const SEP: &'static str = ",";
 const LST: &'static str = ",{";
+const MUL: &'static str = "";
 
 fn base64_decode(value: &mut String) {
     use base64::decode;
@@ -21,10 +22,15 @@ fn base64_decode(value: &mut String) {
     }
 }
 
+fn escape(value: &mut String) {
+    *value = value.replace("\"", "\\\"");
+}
+
 fn main() {
     let collect_params: Vec<String> = env::args().skip(1).collect();
 
     let mut line = String::new();
+    let mut name = String::new();
     let mut value = String::new();
     let mut state = FST;
     let mut encoding = false;
@@ -40,36 +46,62 @@ fn main() {
                 return;
             },
             Ok(1) => {
-                if state == SEP {
-                    if show {
-                        if encoding {
-                            base64_decode(&mut value);
-                        }
-                        print!("\"{}\"", value);
-                        value.clear();
+                if show {
+                    if encoding {
+                        base64_decode(&mut value);
                     }
-                    print!("{}", '}');
-                    state = LST;
+
+                    escape(&mut value);
+                    if state == SEP {
+                        print!("\"{}\"", value);
+                    }
+                    if state == MUL {
+                        print!(",\"{}\"]", value);
+                    }
+                    value.clear();
                 }
+                print!("{}", '}');
+                state = LST;
             },
             Ok(n) => {
                 let line = &line[..n];
                 if line.starts_with('#') {
                     // do nothing
                 } else if !line.starts_with(' ') {
-                    if state == SEP && show {
+                    let (key, val) = line.split_at(line.find(':').unwrap());
+                    if name != key {
+                        if state == SEP && show {
+                            if encoding {
+                                base64_decode(&mut value);
+                            }
+                            escape(&mut value);
+                            print!("\"{}\"", value);
+                        }
+
+                        show = collect_params.is_empty() || collect_params.iter().any(|x| x == key);
+                        if show {
+                            name = key.to_string();
+                            print!("{}\"{}\":", state, name);
+
+                            encoding = val.starts_with("::");
+                            value = val[if encoding { 2 } else { 1 }..].trim().to_string();
+                            state = SEP;
+                        }
+                    } else if show {
                         if encoding {
                             base64_decode(&mut value);
                         }
-                        print!("\"{}\"", value);
-                    }
-                    let (key, val) = line.split_at(line.find(':').unwrap());
-                    show = collect_params.is_empty() || collect_params.iter().any(|x| x == key);
-                    if show {
-                        print!("{}\"{}\":", state, key);
+                        escape(&mut value);
+
+                        if state == SEP {
+                            print!("[\"{}\"", value);
+                        } else {
+                            print!(",\"{}\"", value);
+                        }
+
                         encoding = val.starts_with("::");
                         value = val[if encoding { 2 } else { 1 }..].trim().to_string();
-                        state = SEP;
+                        state = MUL;
                     }
                 } else {
                     value += line.trim();
